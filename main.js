1caba12270489258af01ff069eccb15319270076
Yassin <yassinbennis10@gmail.com>
2018-02-16 10:47:25 -0500

    object and array

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hey It's friday")
+var favCars = [];
+
+ favCars.push("Nissan" , "BMW" , "Lambo" , "Honda" , "Ferrari");
+
+
+ var iLoveFerrari = favCars.pop();
+
+
+
+
+var Clothes = {};
+Clothes.Italian = "Armani";
+Clothes.American = "Levis";
+
+
+console.log(Clothes.Italian);
